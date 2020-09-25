7d33df34b943c30b35ec85bc5a60610b1e0318a2
PRAVIN-SURFACEB <pravinu@gmail.com>
2020-09-25 10:30:09 +0100

    Fetching expenses from database added

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,21 @@
 import configureMockStore from 'redux-mock-store';
-import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense , setExpenses, startSetExpenses} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import thunk from 'redux-thunk';
 import database from '../../firebase/firebase';
+import { now } from 'moment';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) =>{
+  const expenseData={};
+
+  expenses.forEach(({ id, description, amount, note, createdAt}) => {
+    expenseData[id] = { description, amount, note, createdAt};
+  });
+  
+  database.ref('expenses').set(expenseData).then(() => done());
+})
 
 test('should setup remove expense action object', () =>{
   const action = removeExpense({ id: '123abc' });
@@ -88,16 +99,22 @@ test('should add expenses with defaults to database and store', (done) => {
   });;
 });
 
-// test('should setup add expense action with default values', () =>{
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense:{
-//       id: expect.any(String),
-//       description : '',
-//       note :'',
-//       amount : 0,
-//       createdAt : 0
-//     }
-//   });
-// });
+test('should set expenses action object with data',() => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+  });
+});
+
+test('should fetch the expenses from the firebase', (done) => {
+  const store = createMockStore({});
+  store.dispatch(startSetExpenses()).then(()=>{
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type:'SET_EXPENSES',
+      expenses
+    });
+  done();
+  });
+});
